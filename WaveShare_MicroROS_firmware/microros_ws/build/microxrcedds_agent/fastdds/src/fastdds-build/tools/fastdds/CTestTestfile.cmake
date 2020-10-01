bef23309e14ff837ec2c52b67e15e878929fd9c9
MarlarChan <marlarchan.30@gmail.com>
2020-10-01 23:59:49 +0800

    Remove
    Remove

--- WaveShare_MicroROS_firmware/microros_ws/build/microxrcedds_agent/fastdds/src/fastdds-build/tools/fastdds/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/microros/microros_ws/build/microxrcedds_agent/fastdds/src/fastdds/tools/fastdds
-# Build directory: /home/microros/microros_ws/build/microxrcedds_agent/fastdds/src/fastdds-build/tools/fastdds
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
